9a6430d9628ca679796761e55e47ee3b19b4befd
Josh Cormier <jcorm714@gmail.com>
2020-04-22 00:12:04 -0400

    Created Collect-able

---
+++ Assets/Scripts/ModifiersObstacles/CollectableModifier.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableModifier : PlayerModifier
+{
+    // Start is called before the first frame update
+    public override void activate()
+    {
+        GameObject player = this.GetPlayer();
+        player.GetComponent<PlayerScript>().score += 100;
+    }
+
+    public override void Start()
+    {
+        this.timeout = 0;
+    }
+
+    protected override void deactivate()
+    {
+        return;
+    }
+}
---
+++ Assets/Scripts/ModifiersObstacles/CollectableObstacle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableObstacle : Obstacle
+{
+    public override void Start()
+    {
+        this.gameObject.AddComponent<CollectableModifier>();
+        modifiers.Add(this.gameObject.GetComponent<CollectableModifier>());
+    }
+
+    public override void AdditionalEffects()
+    {
+        Destroy(this.gameObject);
+    }
+}

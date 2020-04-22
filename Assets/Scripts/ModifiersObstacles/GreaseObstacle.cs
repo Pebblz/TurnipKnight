36f5b47760b34eb482779d7451a8aa7e2f095741
Josh Cormier <jcorm714@gmail.com>
2020-04-21 23:14:50 -0400

    Created Grease Trap

---
+++ Assets/Scripts/ModifiersObstacles/GreaseObstacle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreaseObstacle : Obstacle
+{
+    public override void Start()
+    {
+        this.gameObject.AddComponent<SpeedyModifier>();
+        this.modifiers.Add(this.gameObject.GetComponent<SpeedyModifier>());
+    }
+}
---
+++ Assets/Scripts/ModifiersObstacles/SpeedyModifier.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedyModifier : PlayerModifier
+{
+    public override void activate()
+    {
+        GameObject player = this.GetPlayer();
+        float newSpeed = player.GetComponent<PlayerScript>().defaultSpeed * 4;
+        player.GetComponent<PlayerScript>().speed = newSpeed;
+    }
+
+    protected override void deactivate()
+    {
+        GameObject player = this.GetPlayer();
+        float newSpeed = player.GetComponent<PlayerScript>().defaultSpeed;
+        player.GetComponent<PlayerScript>().speed = newSpeed;
+    }
+
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        this.timeout = 1f;
+    }
+
+
+}

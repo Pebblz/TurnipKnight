165787ea74a4173d487cd7de04e5b416b2798456
Josh Cormier <jcorm714@gmail.com>
2020-04-22 23:15:31 -0400

    Added Position Bias to camera

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    public float speed = 2.0f;
+    private float speed;
     private GameObject player;
+    public float positionBias;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("PLAYER");
+        speed = player.GetComponent<PlayerScript>().speed;
     }
 
     // Update is called once per frame
@@ -17,7 +19,7 @@ public class CameraScript : MonoBehaviour
     {
         float interp = speed * Time.deltaTime;
         Vector3 pos = this.transform.position;
-        pos.x = Mathf.Lerp(this.transform.position.x, player.transform.position.x, interp);
+        pos.x = Mathf.Lerp(this.transform.position.x, player.transform.position.x , interp) + positionBias * Time.deltaTime;
         //pos.y = Mathf.Lerp(this.transform.position.y, player.transform.position.y, interp);
         this.transform.position = pos;
     

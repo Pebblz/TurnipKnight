ad07c164dfd8c397b7f5fa6c70ac2a25ec382614
James McGurn <jmcgurn@email.neit.edu>
2020-04-26 17:35:30 -0400

    added layouts

---
+++ Assets/Scripts/ModifiersObstacles/SyrupObstacle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SyrupObstacle : Obstacle
+{
+    public override void Start()
+    {
+        this.gameObject.AddComponent<StickyModifier>();
+        this.modifiers.Add(this.gameObject.GetComponent<StickyModifier>());
+    }
+}

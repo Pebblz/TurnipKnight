91067a1264b3e11d8339e69341a0efdf0056c142
Josh Cormier <jcorm714@gmail.com>
2020-04-25 23:19:24 -0400

    Added variable for bounce force

--- Assets/Scripts/ModifiersObstacles/JelloObstacle.cs
+++ Assets/Scripts/ModifiersObstacles/JelloObstacle.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class JelloObstacle : Obstacle
 {
+    public float bounceForce = 4f;
     public override void Start()
     {
         this.gameObject.AddComponent<BouncyModifier>();
+        this.gameObject.GetComponent<BouncyModifier>().bounce = bounceForce;
         this.modifiers.Add(this.gameObject.GetComponent<BouncyModifier>());
         
     }

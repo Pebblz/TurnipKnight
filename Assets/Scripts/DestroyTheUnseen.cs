bf31a86194839e53d498b7976d4ce6a9eea55129
Josh Cormier <jcorm714@gmail.com>
2020-04-27 14:33:07 -0400

    Fixed wall memory leak

---
+++ Assets/Scripts/DestroyTheUnseen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyTheUnseen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        Vector3 dist = cam.WorldToScreenPoint(this.transform.position);
+        if (dist.x < -1 * Screen.width)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}

3c308660198934d3e44e74f091acba176b9c081e
Josh Cormier <jcorm714@gmail.com>
2020-05-02 01:40:15 -0400

    Maybe this works lol

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private static readonly string highScoreFile = @"TurnipKnight/highscore.txt";
+    private static readonly string highScoreFile = @"/TurnipKnight/highscore.bin";
     public static AudioSource soundSource;
     // Start is called before the first frame update
     void Start()
@@ -15,18 +16,72 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private static string GetAndroidExternalDir()
+    {
+        using (AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        {
+            using (AndroidJavaObject context = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
+            {
+                AndroidJavaObject[] externalDirs = context.Call<AndroidJavaObject[]>("getExternalFilesDirs");
+                AndroidJavaObject emulated = null;
+                AndroidJavaObject sdCard = null;
+
+                for (int i = 0; i < externalDirs.Length; i++)
+                {
+                    AndroidJavaObject directory = externalDirs[i];
+                    using (AndroidJavaClass env = new AndroidJavaClass("android.os.Environment"))
+                    {
+                        bool isRemovable = env.CallStatic<bool>("isExternalStorageRemovable", directory);
+                        bool isEmulated = env.CallStatic<bool>("isExternalStorageEmulated", directory);
+
+                        if (isEmulated)
+                        {
+                            emulated = directory;
+                        } else if (isRemovable && isEmulated == false)
+                        {
+                            sdCard = directory;
+                        }
+                    }
+                }
+
+                if (sdCard != null)
+                {
+                    return sdCard.Call<string>("getAbsolutePath");
+                } else
+                {
+                    return emulated.Call<string>("getAbsolutePath");
+                }
+            }
+        }
     }
 
     public static int GetHighScore()
     {
         int highscore = 0;
-
+        string filePath = GetAndroidExternalDir() + highScoreFile;
+        try
+        {
+            using (BinaryReader br = new BinaryReader(File.Open(filePath, FileMode.Open)))
+            {
+                highscore = br.ReadInt32();
+            }
+        }catch(IOException exc)
+        {
+            
+        }
+  
         return highscore;
     }
 
-    public static void SaveHighScore()
+    public static void SaveHighScore(int highscore)
     {
-
+        string filepath = GetAndroidExternalDir() + highScoreFile;
+        using (BinaryWriter wr = new BinaryWriter(File.Open(filepath, FileMode.OpenOrCreate)))
+        {
+            wr.Write(highscore);
+        }
     }
 }
